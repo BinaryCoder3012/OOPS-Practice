3f44ab4358bde3cea6994a85ba2369f7fa62f864
BinaryCoder3012 <lakshya.sudipta15@gmail.com>
2025-12-19 11:57:30 +0530

    StudentDetails.java

--- Classes_And_Objects/StudentDetails.java
+++ Classes_And_Objects/StudentDetails.java
@@ -1,3 +1,22 @@
+// You are tasked with designing a class Student that stores and displays information about students.
+
+// The class must have the following :
+
+
+
+// Attributes :
+
+// name (string) : Stores the name of the student.
+// rollNumber (int) : Stores the roll number of the student
+// Methods :
+
+// setDetails (String name, int rollNumber) : This method initializes the attributes name and rollNumber with the values provided by the user.
+// displayDetails() : This method prints the details of the student in following format (The output consist of two separate lines) :
+
+
+
+
+
 import java.util.*;
 class Student{
     private String name;
@@ -28,4 +47,5 @@ public class StudentDetails{
         student.printDetails();
         scanner.close();
     }
+
 }
